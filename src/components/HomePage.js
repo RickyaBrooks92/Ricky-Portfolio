46ae0b9a6c76e5c8bdb3f846a527ae03674efce9
RickyTicky <Rickyabrooks92@gmail.com>
2022-08-22 15:04:06 -0400

    first commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResponsiveAppBar from './NavBar';
+function HomePage() {
+  return (
+    <div>
+      <div>
+        <ResponsiveAppBar />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
