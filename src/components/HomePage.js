f72c5a3c4094bc1fd5be67ed75dac14ce1a07ed6
RickyTicky <Rickyabrooks92@gmail.com>
2022-08-23 15:48:26 -0400

    added Bio comp and restyled navbar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ResponsiveAppBar from './NavBar';
+import Bio from './Bio';
 function HomePage() {
   return (
     <div>
       <div>
         <ResponsiveAppBar />
+      </div>
+      <div>
+        <Bio />
       </div>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,35 +2,22 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
+
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
+
 import AdbIcon from '@mui/icons-material/Adb';
 
 const pages = ['Projects', 'Tech Stack', 'Contact'];
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
   return (
     <AppBar position='static'>
       <Container maxWidth='xl'>
         <Toolbar disableGutters>
-          <AdbIcon
-            sx={{ display: { xs: 'none', md: 'flex', border: 1 }, mr: 1 }}
-          />
+          <AdbIcon sx={{ display: 'flex', border: 1, mr: 1 }} />
           <Typography
             variant='h6'
             noWrap
@@ -38,7 +25,7 @@ const ResponsiveAppBar = () => {
             href='/'
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
+              display: 'flex',
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
@@ -51,43 +38,12 @@ const ResponsiveAppBar = () => {
           </Typography>
 
           <Box
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', border: 1 } }}
-          >
-            <IconButton
-              size='large'
-              aria-label='account of current user'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
-              onClick={handleOpenNavMenu}
-              color='inherit'
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none', border: 1 },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign='center'>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+            sx={{
+              flexGrow: 1,
+              display: 'flex',
+              justifyContent: 'flex-end',
+            }}
+          ></Box>
           <AdbIcon
             sx={{ display: { xs: 'flex', md: 'none', border: 1 }, mr: 1 }}
           />
@@ -122,7 +78,6 @@ const ResponsiveAppBar = () => {
             {pages.map((page) => (
               <Button
                 key={page}
-                onClick={handleCloseNavMenu}
                 sx={{
                   my: 2,
                   color: 'white',
